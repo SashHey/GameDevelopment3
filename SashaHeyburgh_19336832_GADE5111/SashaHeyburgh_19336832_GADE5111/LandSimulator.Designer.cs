44e7981d5d3f7e8a8f2809697ad8d5ae23caddee
Sasha <60923007+SashHey@users.noreply.github.com>
2022-11-29 21:48:57 +0200

    Add files via upload

---
+++ SashaHeyburgh_19336832_GADE5111/SashaHeyburgh_19336832_GADE5111/LandSimulator.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace SashaHeyburgh_19336832_GADE5111
+{
+    partial class LandSimulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTap2 = new System.Windows.Forms.Button();
+            this.btnTap3 = new System.Windows.Forms.Button();
+            this.btnTap4 = new System.Windows.Forms.Button();
+            this.btnTap5 = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnUntap = new System.Windows.Forms.Button();
+            this.lblTapped = new System.Windows.Forms.Label();
+            this.lblWhite = new System.Windows.Forms.Label();
+            this.lblBlack = new System.Windows.Forms.Label();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnTap1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTap2
+            // 
+            this.btnTap2.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTap2.Location = new System.Drawing.Point(137, 73);
+            this.btnTap2.Name = "btnTap2";
+            this.btnTap2.Size = new System.Drawing.Size(75, 49);
+            this.btnTap2.TabIndex = 1;
+            this.btnTap2.Text = "Tap";
+            this.btnTap2.UseVisualStyleBackColor = true;
+            this.btnTap2.Click += new System.EventHandler(this.btnTap2_Click);
+            // 
+            // btnTap3
+            // 
+            this.btnTap3.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTap3.Location = new System.Drawing.Point(238, 73);
+            this.btnTap3.Name = "btnTap3";
+            this.btnTap3.Size = new System.Drawing.Size(75, 49);
+            this.btnTap3.TabIndex = 2;
+            this.btnTap3.Text = "Tap";
+            this.btnTap3.UseVisualStyleBackColor = true;
+            this.btnTap3.Click += new System.EventHandler(this.btnTap3_Click);
+            // 
+            // btnTap4
+            // 
+            this.btnTap4.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTap4.Location = new System.Drawing.Point(340, 73);
+            this.btnTap4.Name = "btnTap4";
+            this.btnTap4.Size = new System.Drawing.Size(75, 49);
+            this.btnTap4.TabIndex = 3;
+            this.btnTap4.Text = "Tap";
+            this.btnTap4.UseVisualStyleBackColor = true;
+            this.btnTap4.Click += new System.EventHandler(this.btnTap4_Click);
+            // 
+            // btnTap5
+            // 
+            this.btnTap5.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTap5.Location = new System.Drawing.Point(442, 73);
+            this.btnTap5.Name = "btnTap5";
+            this.btnTap5.Size = new System.Drawing.Size(75, 49);
+            this.btnTap5.TabIndex = 4;
+            this.btnTap5.Text = "Tap";
+            this.btnTap5.UseVisualStyleBackColor = true;
+            this.btnTap5.Click += new System.EventHandler(this.btnTap5_Click);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDraw.Location = new System.Drawing.Point(239, 177);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 49);
+            this.btnDraw.TabIndex = 5;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // btnUntap
+            // 
+            this.btnUntap.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUntap.Location = new System.Drawing.Point(622, 208);
+            this.btnUntap.Name = "btnUntap";
+            this.btnUntap.Size = new System.Drawing.Size(119, 49);
+            this.btnUntap.TabIndex = 6;
+            this.btnUntap.Text = "Untap All";
+            this.btnUntap.UseVisualStyleBackColor = true;
+            this.btnUntap.Click += new System.EventHandler(this.btnUntap_Click);
+            // 
+            // lblTapped
+            // 
+            this.lblTapped.AutoSize = true;
+            this.lblTapped.Font = new System.Drawing.Font("Agency FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTapped.Location = new System.Drawing.Point(583, 38);
+            this.lblTapped.Name = "lblTapped";
+            this.lblTapped.Size = new System.Drawing.Size(83, 24);
+            this.lblTapped.TabIndex = 7;
+            this.lblTapped.Text = "Tapped Land:";
+            // 
+            // lblWhite
+            // 
+            this.lblWhite.AutoSize = true;
+            this.lblWhite.Font = new System.Drawing.Font("Agency FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWhite.Location = new System.Drawing.Point(32, 38);
+            this.lblWhite.Name = "lblWhite";
+            this.lblWhite.Size = new System.Drawing.Size(86, 24);
+            this.lblWhite.TabIndex = 8;
+            this.lblWhite.Text = "0 White Mana";
+            // 
+            // lblBlack
+            // 
+            this.lblBlack.AutoSize = true;
+            this.lblBlack.Font = new System.Drawing.Font("Agency FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlack.Location = new System.Drawing.Point(133, 38);
+            this.lblBlack.Name = "lblBlack";
+            this.lblBlack.Size = new System.Drawing.Size(87, 24);
+            this.lblBlack.TabIndex = 9;
+            this.lblBlack.Text = "0 Black Mana";
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Font = new System.Drawing.Font("Agency FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed.Location = new System.Drawing.Point(235, 38);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(78, 24);
+            this.lblRed.TabIndex = 10;
+            this.lblRed.Text = "0 Red Mana";
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.Font = new System.Drawing.Font("Agency FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlue.Location = new System.Drawing.Point(336, 38);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(81, 24);
+            this.lblBlue.TabIndex = 11;
+            this.lblBlue.Text = "0 Blue Mana";
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.AutoSize = true;
+            this.lblGreen.Font = new System.Drawing.Font("Agency FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGreen.Location = new System.Drawing.Point(438, 38);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(91, 24);
+            this.lblGreen.TabIndex = 12;
+            this.lblGreen.Text = "0 Green Mana";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Agency FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(235, 249);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(47, 24);
+            this.lblOutput.TabIndex = 13;
+            this.lblOutput.Text = "Output";
+            // 
+            // btnTap1
+            // 
+            this.btnTap1.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTap1.Location = new System.Drawing.Point(36, 73);
+            this.btnTap1.Name = "btnTap1";
+            this.btnTap1.Size = new System.Drawing.Size(75, 49);
+            this.btnTap1.TabIndex = 14;
+            this.btnTap1.Text = "Tap";
+            this.btnTap1.UseVisualStyleBackColor = true;
+            this.btnTap1.Click += new System.EventHandler(this.btnTap1_Click_1);
+            // 
+            // LandSimulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 291);
+            this.Controls.Add(this.btnTap1);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.lblBlack);
+            this.Controls.Add(this.lblWhite);
+            this.Controls.Add(this.lblTapped);
+            this.Controls.Add(this.btnUntap);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.btnTap5);
+            this.Controls.Add(this.btnTap4);
+            this.Controls.Add(this.btnTap3);
+            this.Controls.Add(this.btnTap2);
+            this.Name = "LandSimulator";
+            this.Text = "Land Simulator";
+            this.Load += new System.EventHandler(this.LandSimulator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnTap2;
+        private System.Windows.Forms.Button btnTap3;
+        private System.Windows.Forms.Button btnTap4;
+        private System.Windows.Forms.Button btnTap5;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnUntap;
+        private System.Windows.Forms.Label lblTapped;
+        private System.Windows.Forms.Label lblWhite;
+        private System.Windows.Forms.Label lblBlack;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblBlue;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnTap1;
+    }
+}
+
---
+++ SashaHeyburgh_19336832_GADE5111/SashaHeyburgh_19336832_GADE5111/LandSimulator.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SashaHeyburgh_19336832_GADE5111
+{
+    public partial class LandSimulator : Form
+    {
+        public int randomNumber;                                        //variable initialization
+        int[] generatedMana = new int[Constant.colourNumbers];
+        int[] tappedManaPool = new int[Constant.colourNumbers];
+        List<string> colours = new List<string>() {" White Mana", " Black Mana", " Red Mana", " Blue Mana", " Green Mana"};
+
+        public static class Constant
+        {
+            public const int colourNumbers = 5;                             //constant declaration
+        }
+
+        public LandSimulator()
+        {
+            InitializeComponent();
+        }
+
+        private void LandSimulator_Load(object sender, EventArgs e)
+        {
+            int[] generatedMana = new int[Constant.colourNumbers];          //array initialization
+            int[] tappedManaPool = new int[Constant.colourNumbers];
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            Random numberColours = new Random();
+            randomNumber = numberColours.Next(Constant.colourNumbers + 1);          //randomization
+
+            DoSwitch();                                             //calling the switch function
+        }
+
+        private void DoSwitch()                         //switch function for the mana colour pool
+        {
+            switch(randomNumber)
+            {
+                case 0:
+
+                    lblWhite.Text = (generatedMana[0] += 1) + " White Mana";                  
+                    break;
+
+                case 1:
+
+                    lblBlack.Text = (generatedMana[1] += 1) + " Black Mana";
+                    break;
+
+                case 2:
+
+                    lblRed.Text = (generatedMana[2] += 1) + " Red Mana";
+                    break;
+
+                case 3:
+
+                    lblBlue.Text = (generatedMana[3] += 1) + " Blue Mana";
+                    break;
+
+                case 4:
+
+                    lblGreen.Text = (generatedMana[4] += 1) + " Green Mana";
+                    break;
+
+                case 5:
+
+                    lblOutput.Text = "You drew a creature. No mana was added to the pool";
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void btnTap2_Click(object sender, EventArgs e)          //black mana tap button
+        {
+            if (generatedMana[1] == 0)
+            {
+                lblTapped.Text = "Tapped Land: \r\n There is no mana to use";
+            }
+            else
+            {
+                lblBlack.Text = (generatedMana[1] -= 1) + " Black Mana";
+                tappedManaPool[1] += 1;                                                             //moving generatedMana array values to tappedManaPool array
+                lblOutput.Text = "You have used one black mana";
+                lblTapped.Text = "Tapped Land: ";
+            }
+
+            if (tappedManaPool[1] == 0)
+            {
+                //nothing
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)                                                                         //looping through the tappedManaPool array
+                    if (tappedManaPool[i] == 0)
+                    {
+                        //nothing
+                    }
+                    else
+                    {
+                        lblTapped.Text = lblTapped.Text + "\r\n" + tappedManaPool[i] + " " + colours[i];            //adding colours to the Tapped Land label
+                    }
+            }
+        }
+
+        private void btnTap1_Click_1(object sender, EventArgs e)            //white mana tap button
+        {
+            if (generatedMana[0] == 0)
+            {
+                lblTapped.Text = "Tapped Land: \r\n There is no mana to use";
+            }
+            else
+            {
+                lblWhite.Text = (generatedMana[0] -= 1) + " White Mana";
+                tappedManaPool[0] += 1;                                                             //moving generatedMana array values to tappedManaPool array
+                lblOutput.Text = "You have used one white mana";
+                lblTapped.Text = "Tapped Land: ";
+            }
+
+            if (tappedManaPool[0] == 0)
+            {
+                //nothing
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)                                                                         //looping through the tappedManaPool array
+                    if (tappedManaPool[i] == 0)
+                    {
+                        //nothing
+                    }
+                    else
+                    {
+                        lblTapped.Text = lblTapped.Text + "\r\n" + tappedManaPool[i] + " " + colours[i];            //adding colours to the Tapped Land label
+                    }
+            }
+        }
+
+        private void btnTap3_Click(object sender, EventArgs e)          //red mana tap button
+        {
+            if (generatedMana[2] == 0)
+            {
+                lblTapped.Text = "Tapped Land: \r\n There is no mana to use";
+            }
+            else
+            {
+                lblRed.Text = (generatedMana[2] -= 1) + " Red Mana";
+                tappedManaPool[2] += 1;                                                             //moving generatedMana array values to tappedManaPool array
+                lblOutput.Text = "You have used one red mana";
+                lblTapped.Text = "Tapped Land: ";
+            }
+
+            if (tappedManaPool[2] == 0)
+            {
+                //nothing
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)                                                                         //looping through the tappedManaPool array
+                    if (tappedManaPool[i] == 0)
+                    {
+                        //nothing
+                    }
+                    else
+                    {
+                        lblTapped.Text = lblTapped.Text + "\r\n" + tappedManaPool[i] + " " + colours[i];            //adding colours to the Tapped Land label
+                    }
+            }
+        }
+
+        private void btnTap4_Click(object sender, EventArgs e)          //blue mana tap button
+        {
+            if (generatedMana[3] == 0)
+            {
+                lblTapped.Text = "Tapped Land: \r\n There is no mana to use";
+            }
+            else
+            {
+                lblBlue.Text = (generatedMana[3] -= 1) + " Blue Mana";
+                tappedManaPool[3] += 1;                                                             //moving generatedMana array values to tappedManaPool array
+                lblOutput.Text = "You have used one blue mana";
+                lblTapped.Text = "Tapped Land: ";
+            }
+
+            if (tappedManaPool[3] == 0)
+            {
+                //nothing
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)                                                                         //looping through the tappedManaPool array
+                    if (tappedManaPool[i] == 0)
+                    {
+                        //nothing
+                    }
+                    else
+                    {
+                        lblTapped.Text = lblTapped.Text + "\r\n" + tappedManaPool[i] + " " + colours[i];            //adding colours to the Tapped Land label
+                    }
+            }
+        }
+
+        private void btnTap5_Click(object sender, EventArgs e)          //green mana tap button
+        {
+            if (generatedMana[4] == 0)
+            {
+                lblTapped.Text = "Tapped Land: \r\n There is no mana to use";
+            }
+            else
+            {
+                lblGreen.Text = (generatedMana[4] -= 1) + " Green Mana";
+                tappedManaPool[4] += 1;                                                             //moving generatedMana array values to tappedManaPool array
+                lblOutput.Text = "You have used one green mana";
+                lblTapped.Text = "Tapped Land: ";
+            }
+
+            if (tappedManaPool[4] == 0)
+            {
+                //nothing
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)                                                                         //looping through the tappedManaPool array
+                    if (tappedManaPool[i] == 0)
+                    {
+                        //nothing
+                    }
+                    else
+                    {
+                        lblTapped.Text = lblTapped.Text + "\r\n" + tappedManaPool[i] + " " + colours[i];            //adding colours to the Tapped Land label
+                    }
+            }
+        }
+
+        private void btnUntap_Click(object sender, EventArgs e)                                 //moving tapped mana array values back to generated mana array
+        {
+            lblWhite.Text = generatedMana[0] + tappedManaPool[0] + " White Mana";
+            generatedMana[0] += tappedManaPool[0];
+
+            lblBlack.Text = generatedMana[1] + tappedManaPool[1] + " Black Mana";
+            generatedMana[1] += tappedManaPool[1];
+
+            lblRed.Text = generatedMana[2] + tappedManaPool[2] + " Red Mana";
+            generatedMana[2] += tappedManaPool[2];
+
+            lblBlue.Text = generatedMana[3] + tappedManaPool[3] + " Blue Mana";
+            generatedMana[3] += tappedManaPool[3];
+
+            lblGreen.Text = generatedMana[4] + tappedManaPool[4] + " Green Mana";
+            generatedMana[4] += tappedManaPool[4];
+
+            Array.Clear(tappedManaPool, 0, tappedManaPool.Length);
+            lblTapped.Text = "Tapped Land: \r\n You have untapped all your land";
+        }
+    }
+}
